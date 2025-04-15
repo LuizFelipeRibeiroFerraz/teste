b68a19a001e95027f65c3c8f2d9553ae6ba072b1
Nunno Wakiyama <nunno.00000849115@unicap.br>
2025-04-15 17:45:43 -0300

    Nao consigo aaaa

---
+++ boanoite.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Morra em paz" << endl;
+    return 0;
+}
---
+++ bomdia.py
@@ -0,0 +1,4 @@
+print("Morra em paz")
+c = int(input())
+d = 1+2+c
+print(d)
